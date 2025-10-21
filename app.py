b29ec499813f4bc8c1480a11d94c166388d545bf
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-10-21 19:39:33 +0100

    building the endpoint for the model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status, HTTPException, Depends
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
@@ -7,6 +7,9 @@ from passlib.context import CryptContext
 import os
 import traceback
 from fastapi.responses import JSONResponse
+import joblib
+import pandas as pd
+import numpy as np
 
 
 app = FastAPI()
@@ -38,6 +41,8 @@ class UserDb(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    position = Column(String, nullable=True)  
+    Department = Column(String, nullable=True)
 
 #Base.metadata.create_all(bind=engine)   #---- create table
 
@@ -46,6 +51,8 @@ class UserCreate(BaseModel): # --request model
     name: str
     email : str
     password: str
+    position: str
+    Department: str
 
 class UserResponse(BaseModel): # -- response model
     id: int
@@ -58,6 +65,30 @@ class UserLogin(BaseModel):
     email: str
     password: str
 
+class UserProfileRequest(BaseModel):
+    name: str
+    email: str
+
+class UserProfileResponse(BaseModel):
+    name: str
+    email: str
+    position: str
+    Department: str
+
+class UserFeaturesRequest(BaseModel):
+    country: str = Field(alias="Country")
+    year: int = Field(alias="Year")
+    no_of_cases_median: int = Field(alias="No. of cases_median")
+    no_of_cases_min: int = Field(alias="No. of cases_min")
+    no_of_cases_max: int = Field(alias="No. of cases_max")
+    no_of_deaths_median: int = Field(alias="No. of deaths_median")
+    no_of_deaths_min: int = Field(alias="No. of deaths_min")
+    no_of_deaths_max: int = Field(alias="No. of deaths_max")
+    who_region: str = Field(alias="WHO Region")
+    case: int = Field(alias="case")
+
+class UserTragetResponse(BaseModel):
+    death: int
 
 def get_db():
     db = sessionlocal()
@@ -74,7 +105,7 @@ def on_startup():
 
 
 
-@app.post("/users/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@app.post("/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED, include_in_schema=True)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
         # Check if user already exists
@@ -87,7 +118,10 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         new_user = UserDb(
             name=user.name,
             email=user.email,
-            hashed_password=hashed_password
+            hashed_password=hashed_password,
+            position = user.position,
+            Department= user.Department
+
         )
         db.add(new_user)
         db.commit()
@@ -117,3 +151,40 @@ def user_login(user: UserLogin, db: Session = Depends(get_db)):
         # 3. Return success message
         return {"message": f"Welcome back, {db_user.name}!"}
     
+
+@app.post("/profile", response_model=UserProfileResponse, status_code=status.HTTP_200_OK)
+def check_profile(user: UserProfileRequest, db: Session = Depends(get_db)):
+    db_user = db.query(UserDb).filter(UserDb.email == user.email).first()
+    if not db_user:
+        raise HTTPException(status_code=400, detail="pls login to acess your profile")
+    
+    profile = {
+        "name": db_user.name,
+        "email": db_user.email,
+        "position": db_user.position,
+        "Department": db_user.Department
+    }
+
+    return profile
+
+
+@app.post("/predict", response_model=UserTragetResponse,status_code=status.HTTP_200_OK)
+def get_predictions(features: UserFeaturesRequest):
+    try:
+
+         model = joblib.load("malaria_death_mmodel.pkl")
+
+        # Convert input to DataFrame using the Pydantic model's dict (with aliases)
+         input_data = pd.DataFrame([features.model_dump(by_alias=True)])
+
+        # Make prediction
+         prediction = model.predict(input_data)
+
+        # Return the prediction
+         return {"death": int(prediction[0])}
+
+    except Exception as e:
+    
+        print("Prediction error:", e)
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {e}")
+
